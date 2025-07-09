5537d345d8d16461d6610715d18f67704a29b1b8
akshatvermavi <akshatvermanike@gmail.com>
2025-07-09 15:54:16 +0530

    security configuration done for routing and api end point access

---
+++ src/main/java/com/akshat/vimeo/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.akshat.vimeo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers( "/service/video/**").permitAll()
+                        //.anyRequest().authenticated()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
+
+}
